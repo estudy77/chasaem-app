a91372069c0edb5504e06069835d7cd3194dc6ef
PC <lovelike77@naver.com>
2026-09-10 15:39:26 +0900

    update 2026-09-10 15:39:26

--- push-all.bat
+++ push-all.bat
@@ -1,18 +1,2 @@
 @echo off
-cd /d "%~dp0"
-
-echo == git commit ==
-git add -A
-git commit -m "update %date% %time%"
-
-echo.
-echo == git push (GitHub 백업) ==
-git push
-
-echo.
-echo == clasp push (구글 Apps Script 반영) ==
-clasp push --force
-
-echo.
-echo 완료됐습니다.
-pause
+powershell -NoProfile -ExecutionPolicy Bypass -File "%~dp0push-all.ps1"
